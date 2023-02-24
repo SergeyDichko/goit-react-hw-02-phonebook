acaee1c340786eff7c8d8e89cbdf426b6808c5d5
SergeyDichko <hillsmen94@gmail.com>
2023-02-24 21:40:56 +0200

    add

---
+++ src/components/Form/Form.styled.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+
+export const Input = styled.input`
+display: block;
+margin-bottom: 20px;
+
+
+`;
+
+export const BtnAddContact = styled.button`
+
+margin-bottom: 20px;
+
+`
